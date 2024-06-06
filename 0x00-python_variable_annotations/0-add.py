a3b0c4ef1f8b785774bf321b9f3c23d091988a05
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-06 17:33:11 +0300

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Python type-annotated function add
+that takes a float a and a float b as
+arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    The function add.
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+This project entails variable annotations in Python
