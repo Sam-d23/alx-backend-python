62e1a073fa549c2b0ed7c22d07117677c0e29815
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-06 20:38:40 +0300

    task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -6,8 +6,8 @@ as arguments and returns a concatenated string.
 """
 
 
-def concat(str: str1, str: str2) -> str:
+def concat(str1: str, str2: str) -> str:
     """
     Function concat.
     """
-    return str1 + str2 
+    return str1 + str2
