9a4c6952760daf797c19435d0556007aaa3c6ac0
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-10 14:08:54 +0300

    task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,6 +4,10 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """
+    Asynchronous co-routine that takes in an argument,
+    that waits for a randomn delay and returns it.
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
